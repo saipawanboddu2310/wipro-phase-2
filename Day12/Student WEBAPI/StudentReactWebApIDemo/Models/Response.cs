745e2c68f0f3620bff4ec8f70ad7259f261caae0
saipawanboddu2310 <saipawanboddu2310@gmail.com>
2025-03-19 05:29:20 +0530

    Student WebApi

---
+++ Day12/Student WEBAPI/StudentReactWebApIDemo/Models/Response.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentReactWebApIDemo.Models
+{
+    public class Response
+    {
+
+
+        public string? Status { get; set; }
+        public string? Message { get; set; }
+
+    }
+}
---
+++ Day12/Student WEBAPI/StudentReactWebApIDemo/Models/StudentDto.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentReactWebApIDemo.Models
+{
+    public class StudentDto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public IFormFile ImageFile { get; set; }
+    }
+}
